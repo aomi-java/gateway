5df7c434fd72b7364337a3663c438adc86bea1d0
尼古拉斯丶少 <sean.snow@live.com>
2021-06-25 17:30:01 +0800

    feat: 启用缓存

--- src/main/java/tech/aomi/cloud/gateway/GatewayApplication.java
+++ src/main/java/tech/aomi/cloud/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package tech.aomi.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author Sean createAt 2021/5/7
  */
+@EnableCaching
 @EnableConfigurationProperties({GatewayProperties.class})
 @SpringBootApplication
 public class GatewayApplication {
